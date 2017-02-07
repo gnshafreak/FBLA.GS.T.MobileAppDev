3f4bcc3ed4505747e88f6f7c8b1627afd28db481
GabeSha <gnshafiq@gmail.com>
2017-02-07 15:54:33 -0500

    This is some stuff we added, not sure

---
+++ app.java
@@ -0,0 +1,21 @@
+/*Digital Yard Sale to Attend NLC
+ * 	Donation of Items - File upload applet packet from "http://stackoverflow.com/questions/1050964/how-to-do-file-uploads-via-a-java-applet"
+ * 		Picture
+ * 		Suggested Price
+ * 		Rating System of Condition
+ * 	Interactions/Comments on Items
+ * ~Basically a Very Simple Amazon~
+ */
+import java.util.*;
+import java.awt.*;
+import java.applet.*;
+
+
+public class app extends Applet{
+	public void paint (Graphics g){
+		g.drawString("NLC \"Digital Yard Sale\"", 80, 80);
+		
+	}
+
+	
+}
